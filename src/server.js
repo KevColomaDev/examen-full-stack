3d6a466cb8989d3ce304a0aa59a8e81e760246c3
KevColomaDev <kev.enrri@hotmail.com>
2024-07-21 16:41:42 -0500

    fix: Cors Frontend

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import e from 'express'
 import { router } from './routers/administrators.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 export const app = e()
 
 // Middlewares
+app.use(cors())
 app.use(e.json())
 app.use(cookieParser())
 
