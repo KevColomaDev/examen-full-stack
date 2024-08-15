bbf8f93d3cf7c6ae67cd35d138a08aa679aa5a02
KevColomaDev <kev.enrri@hotmail.com>
2024-08-15 00:30:20 -0500

    update: supplies register supported 50

--- src/schemas/registerSupplies.js
+++ src/schemas/registerSupplies.js
@@ -1,10 +1,10 @@
 import z from 'zod'
 
 const registerSupliesSchema = z.object({
-  toothPaste: z.number().min(0).max(9).optional(),
-  soap: z.number().min(0).max(9).optional(),
-  toothBrush: z.number().min(0).max(9).optional(),
-  towel: z.number().min(0).max(9).optional()
+  toothPaste: z.number().min(0).max(50).optional(),
+  soap: z.number().min(0).max(50).optional(),
+  toothBrush: z.number().min(0).max(50).optional(),
+  towel: z.number().min(0).max(50).optional()
 })
 export const validateRegisterSupplies = (data) => {
   const isValid = registerSupliesSchema.safeParse(data)
